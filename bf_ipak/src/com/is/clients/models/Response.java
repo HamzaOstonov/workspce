e044b6111cb246f27f2cb183d7f6423ef28565d4
hamza <hamza_ost@mail.ru>
2024-11-25 22:48:30 +0500

    1

--- bf_ipak/src/com/is/clients/models/Response.java
+++ bf_ipak/src/com/is/clients/models/Response.java
@@ -3,20 +3,65 @@ package com.is.clients.models;
 public class Response {
 	 private String client;
 	 private String client_type;
-	 private String client_state;
+	 private String client_bank;
+	 private String client_branch;
+	 private String account_bank;
+	 private String account_branch;
+	 private String account_office;
 	 private String account_state;
+	 private String client_state;
 	 private String name;
 	 private String opened;
 	 private String closed;
 
-
 	 // Getter Methods 
 
+	 
 	 public String getClient() {
 	  return client;
 	 }
 
-	 public String getClient_type() {
+	 public String getClient_bank() {
+		return client_bank;
+	}
+
+	public void setClient_bank(String client_bank) {
+		this.client_bank = client_bank;
+	}
+
+	public String getClient_branch() {
+		return client_branch;
+	}
+
+	public void setClient_branch(String client_branch) {
+		this.client_branch = client_branch;
+	}
+
+	public String getAccount_bank() {
+		return account_bank;
+	}
+
+	public void setAccount_bank(String account_bank) {
+		this.account_bank = account_bank;
+	}
+
+	public String getAccount_branch() {
+		return account_branch;
+	}
+
+	public void setAccount_branch(String account_branch) {
+		this.account_branch = account_branch;
+	}
+
+	public String getAccount_office() {
+		return account_office;
+	}
+
+	public void setAccount_office(String account_office) {
+		this.account_office = account_office;
+	}
+
+	public String getClient_type() {
 	  return client_type;
 	 }
 
